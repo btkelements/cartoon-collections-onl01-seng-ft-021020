3dd595a54c2d1938f7166b1e82ec7518d41f6d15
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-19 18:25:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,9 @@ def roll_call_dwarves(dwarves)
     puts "#{index}. #{name}"
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+    call.
 end
 
 def long_planeteer_calls# code an argument here
